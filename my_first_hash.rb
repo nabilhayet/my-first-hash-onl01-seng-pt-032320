24c19631f372dbd7c11381dc498e65d2494ec0f8
Nabil Hayet <nabil.bracu@gmail.com>
2020-03-29 18:59:56 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,8 +3,7 @@ def my_hash
   
   your_hash={}
   your_hash["name"]="Nabil"
-
-  
+  your_hash
 end
 
 
@@ -12,6 +11,7 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  
 
 
 end
