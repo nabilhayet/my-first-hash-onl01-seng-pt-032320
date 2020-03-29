1dff66a71ea9bed34341c701cc4313222b6a5c47
Nabil Hayet <nabil.bracu@gmail.com>
2020-03-29 19:05:58 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,10 +15,8 @@ def shipping_manifest
   the_manifest={}
   the_manifest["whale bone corsets"]=5
   the_manifest["porcelain vases"]=2
-  the_manifest[""]=3
-  
-
-
+  the_manifest["oil paintings"]=3
+  the_manifest
 end
 
 def retrieval
@@ -29,6 +27,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest["oil paintings"]
 
 end
 
@@ -39,7 +38,9 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+shipping_manifest["muskets"]=2
+shipping_manifest["gun powder"]=4
+shipping_manifest
 
 
   # add 4 gun powder to the shipping_manifest hash below
